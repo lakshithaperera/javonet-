85785b73f357dc78e7583830c80b64c6daf24e9f
lakshithaperera <pasindulakshitha33@gmail.com>
2024-04-23 12:39:27 +0530

    first commit

--- src/components/nav-bar-signin/NavBarSignIn.jsx
+++ src/components/nav-bar-signin/NavBarSignIn.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import "./navbarlogin.scss"
 import logo from "../../assets/Logo.svg"
 import en_icon from "../../assets/en-icn.svg"
+import { useNavigate } from 'react-router-dom';
 
 export const NavBarSignIn = () => {
+  const navigate = useNavigate();
+    
+  const navigateToSignIn = () => {
+      navigate("/sign-in", {});
+     };
   return (
     <nav className='nav-bar-login'>
          <div className="wrapper">
@@ -11,7 +17,7 @@ export const NavBarSignIn = () => {
                     <img src={logo} alt="" />
                 </div>
                 <div className="btns">
-                    <button className='login-btn'>LOG IN</button>
+                    <button onClick={navigateToSignIn} className='login-btn'>LOG IN</button>
                     <span></span>
                     <button className='en-btn'>EN <img src={en_icon} alt="icon" /></button>
                 </div>
