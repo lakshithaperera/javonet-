3684f33acdc2ceddfe39eeaf7dab611a7a46cdf2
lakshithaperera <pasindulakshitha33@gmail.com>
2024-04-23 13:51:10 +0530

    4 comm

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
            <Route path="sign-in" element={<SignIn />}></Route>
            <Route path="create-account" element={<CreateAccount />}></Route>
            <Route
-        path="/"
+        path="project"
         element={<ProjectName setProjectEmails={setProjectEmails} />}
       />
       <Route path="summary" element={<Summary projectEmails={projectEmails} />} />
